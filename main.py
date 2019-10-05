c436c14c4e38c48cd49103f74a1d75b11df1c9fb
linkghm <link2619@126.com>
2019-10-05 14:46:51 +0800

    put labels into list

--- main.py
+++ main.py
@@ -4,15 +4,17 @@
 # @Email   : guohuimin2619@foxmail.com
 # @FileName: main.py
 
-from pathConfig import pathConfig
+from pathConfig import pathConfig, pathConfig_local
 from showUtils import *
 
 if __name__ == '__main__':
-    numpy_image = pathConfig["numpy_image_path"]
-    labels = np.load(pathConfig["label_path"])
+    numpy_image = pathConfig_local["numpy_image_path"]
+    labels = np.load(pathConfig_local["label_path"])
     info = []
+    labels = [labels]
     for label in labels:
         z, y, x, diameter_y, diameter_x = label[0], label[1], label[2], label[4], \
                                           label[5]
         info.append((z, y, x, diameter_y, diameter_x))
+
     showPosition(image_path=numpy_image, info=info)
